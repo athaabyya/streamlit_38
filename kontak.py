ddb23af898914078bb0056d322ace869ebdecd00
athaabyya <najmiatha06@gmail.com>
2026-02-04 14:55:46 +0700

    first_code

---
+++ kontak.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def munculkan_kontak():
+    st.title("Kontak")
+    st.write("Hibungi saya melalui teutan berikut:")
+
+      # LinkedIn
+    st.markdown(
+        "🔗 **LinkedIn**  \n"
+        "[https://www.linkedin.com/in/NajmiAthaAbyya](https://www.linkedin.com/in/najmi-atha-abyya)"
+    )
+
+    # GitHub
+    st.markdown(
+        "💻 **GitHub**  \n"
+        "[https://github.com/NajmiAthaAbyya](https://github.com/athaabyya)"
+    )
+
+    # Email
+    st.write(" Email: najmiatha06@gmail.com")
---
+++ tentang.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def about_me():
+    st.subheader('Nama saya adalah Abyya')
+    st.write('Saya seorang Data Scientist')
